1e8cf0e49822968a8f698ab99623cde5f31f1ff1
Mykola Dziuba <mykola.dziuba92@gmail.com>
2021-08-26 15:10:29 +0200

    backup-26-08-2021

--- backup.sh
+++ backup.sh
@@ -2,30 +2,25 @@
 PATH=$PATH:$HOME/bin:/usr/sbin:/usr/bin:/script
 export PATH
 
+#Create backup tar file 
 title='save-var-'`date +\%d-\%m-\%Y`'.tar.gz'
 tar -czvf $title /var/log/*.gz /var/mail
 
-#Use gupload to upload the archive to my Google Drive
+#Upload the archive to Google Drive via rclone
 rclone copy $title remote:backup 
 
 #Delete the archive
 rm $title
 
-#Print end status message
-#echo "Backup finished"
-
 #Checking and test
-#Download file from Google Drive 
+#Download file from Google Drive via rclone
 rclone sync remote:backup /home/DevOps/backup
 
-#Print end status message
-#echo "Unbackup finished"
-#date
+#Create raport
+tar -ztvf /home/DevOps/backup/$title > /home/DevOps/backup/raport-`date +\%d-\%m-\%Y`  
 
-#Add to github
+#Add backup to github
 git -C /home/DevOps add .
 git -C /home/DevOps commit -m "backup-`date +\%d-\%m-\%Y`"
 git -C /home/DevOps push origin
 
-#Create raport
-tar -ztvf /home/DevOps/backup/$title > /home/DevOps/backup/raport-`date +\%d-\%m-\%Y`  
